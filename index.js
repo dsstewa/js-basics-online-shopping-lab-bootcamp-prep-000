7157533fd12f7b21b9384ed9102bb68b503a93c1
David Stewart <davidstewart@gmail.com>
2019-01-05 19:38:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function viewCart() {
   for (var i = 0; i < cart.length; i++ ) {
   var array2Object = cart[i]
   
-  cartPrint[0]= cartPrint +`${array2Object.itemName} at`
+  cartPrint[0]= cartPrint +` ${array2Object.itemName} at`
   
   
 
