251952b1c07605c834b2ed9969a1e286fce5ca64
David Stewart <davidstewart@gmail.com>
2019-01-05 21:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,11 +89,12 @@ return cart
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    var cost = total()
+    cart = [];
+    return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
- cart = [];
+ 
 }
-cart = [];
