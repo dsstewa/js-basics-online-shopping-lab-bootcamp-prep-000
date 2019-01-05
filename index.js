06ba42fa20168d53a5f32f68123c37e89fdc9df9
David Stewart <davidstewart@gmail.com>
2019-01-05 20:14:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,8 +38,6 @@ function viewCart() {
   
 } else {
   
-}
-  
   for (var i = 0; i < cart.length; i++) {
   var array2Object = cart[i];
   console.log(array2Object.itemName)
@@ -47,9 +45,8 @@ function viewCart() {
   cartPrint[0]= cartPrint +` ${array2Object.itemName} at $${array2Object.itemPrice}.`
   console.log(i)
   
-    
   }
- 
+}
   return cartPrint
   
   }
